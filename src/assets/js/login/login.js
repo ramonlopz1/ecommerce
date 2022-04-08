e7ebf1460c8c0135a826dc2f57a2ad888e9dd2b5
ramonlopz1 <rmnprojetos@outlook.com>
2022-04-08 12:05:19 -0300

    addProdutos page

--- src/assets/js/login/login.js
+++ src/assets/js/login/login.js
@@ -1,50 +1,16 @@
-const formLogin = document.querySelector("#form__login")
-const btnAcessar = document.querySelector("#login__form__submit")
+
+
 const btnCadastrar = document.querySelector("#login__form__register")
 
-const loginDiv = document.querySelector("#form__input__user")
-const passDiv = document.querySelector("#form__input__pass")
+const mostrarInputsCadastro = () => {
+    const inputEmail = document.querySelector("#form__input__email")
+    const inputRepeatPass = document.querySelector("#form__input__repeatpass")
 
-
-const buildElements = () => {
-// Cria div
-    const emailDiv = document.createElement("div")
-    emailDiv.setAttribute("id", "form__input__email")
-
-    // Cria label
-    const emailDivLabel = document.createElement("label")
-    emailDivLabel.setAttribute("for", "input_email")
-
-    const emailDivLabelIcon = document.createElement("i")
-    emailDivLabelIcon.classList.add("fa-solid")
-    emailDivLabelIcon.classList.add("fa-envelopes-bulk")
-    
-
-    // Cria input
-    const emailDivInput = document.createElement("input")
-    emailDivInput.setAttribute("id", "login__form__email")
-    emailDivInput.setAttribute("placeholder", "Insira seu e-mail")
-    //
-
-    emailDiv.appendChild(emailDivLabel)
-    emailDivLabel.appendChild(emailDivLabelIcon)
-    emailDiv.appendChild(emailDivInput)
-
-    loginDiv.insertAdjacentElement("beforebegin", emailDiv)
+    inputEmail.style.display = "flex"
+    inputRepeatPass.style.display = "flex"
 }
 
-let inputQtd = 0;
-
-const Register = (event) => {
+btnCadastrar.addEventListener('click', event => {
     event.preventDefault()
-    
-    if (inputQtd === 0) {
-        buildElements()
-        inputQtd++;
-    }
-}
-
-btnCadastrar.addEventListener('click', (event) => {
-    Register(event)
+    mostrarInputsCadastro()
 })
-
