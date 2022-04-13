28c63bf8d2d9f63c29e30a6c3c31800e933a0450
ramonlopz1 <rmnprojetos@outlook.com>
2022-04-12 21:54:58 -0300

    crud com fetch

---
+++ src/controller/novoProduto.js
@@ -0,0 +1,38 @@
+import { httpMethods } from "../services/service-httpmethods.js"
+
+const novoProduto = () => {
+    const produtoImg = document.querySelector("#produtoimginput")
+    const produtoNome = document.querySelector("#produtonome")
+    const produtoPreco = document.querySelector("#produtopreco")
+    const produtoDescricao = document.querySelector("#produtodescricao")
+    
+    /*const produtos = JSON.parse(localStorage.getItem("produtos")) || []*/
+
+    const novoProduto = {
+        img: produtoImg.value,
+        nome: produtoNome.value,
+        preco: produtoPreco.value,
+        descricao: produtoDescricao.value
+    }
+
+   /* const atualizaProdutos = [...produtos, novoProduto]*/
+
+    if(novoProduto.img && novoProduto.nome && 
+        novoProduto.preco && novoProduto.descricao) {
+        /**localStorage.setItem('produtos', JSON.stringify(atualizaProdutos)) */
+
+        httpMethods.criaProduto(novoProduto)
+
+    } else {
+        console.log("preencha todos os campos")
+    }
+
+    document.forms[0].reset()
+}
+
+const btnAdd = document.querySelector("#btns__cadastrar")
+
+btnAdd.addEventListener("click", (event) => {
+    event.preventDefault()
+    novoProduto()
+})
