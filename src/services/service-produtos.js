16aa15087cd063f8c1b6fb311cf085af4b2c4f78
ramonlopz1 <rmnprojetos@outlook.com>
2022-05-25 18:56:43 -0300

    server db.json github

--- src/services/service-produtos.js
+++ src/services/service-produtos.js
@@ -1,36 +1,49 @@
 const getProdutos = async () => {
-    const resp = await fetch("https://ramonlopz1.github.io/ecommerce/db.json")
-    const dados = await resp.json()
-    return dados.produtos
+    try {
+        const resp = await fetch("https://ramonlopz1.github.io/ecommerce/db.json")
+        const dados = await resp.json()
+        return dados.produtos
+    } catch (e) {
+        console.error("Erro encontrado: " + e.message)
+    }
 }
 
-const postProdutos = (dados) => { 
+const postProdutos = (dados) => {
     return fetch(`http://localhost:3000/produtos`, {
-        method: 'POST', 
-        headers: { 'Content-Type' : 'application/json' },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(dados)
     })
 }
 
 const getProduto = async (id) => {
-    const resp = await fetch(`https://ramonlopz1.github.io/ecommerce/db.json`)
-    const dados = await resp.json()
-    const produtos = dados.produtos
-    return produtos[id]
+
+    try {
+        const resp = await fetch(`https://ramonlopz1.github.io/ecommerce/db.json`)
+        const dados = await resp.json()
+        const produtos = dados.produtos
+        return produtos[id++] // incrementa 1 pois o índice começa em 0 e o id do produto começa em 1
+    } catch (e) {
+        console.error("Erro encontrado: " + e.message)
+    }
 }
 
-const putProdutos = async (dados, id) => { 
-    const resp = await fetch(`http://localhost:3000/produtos/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(dados)
-    })
-    return await resp.json()
+const putProdutos = async (dados, id) => {
+    try {
+        const resp = await fetch(`http://localhost:3000/produtos/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dados)
+        })
+        return await resp.json()
+    } catch (e) {
+        console.error("Erro encontrado: " + e.message)
+    }
 }
 
 const deleteProduto = (id) => {
     return fetch(`http://localhost:3000/produtos/${id}`, {
-        method: 'DELETE', 
+        method: 'DELETE',
     })
 }
 
@@ -38,7 +51,7 @@ const uploadProdutos = (dados) => {
     return fetch(dados.url, {
         method: 'POST',
         body: dados.formData,
-        
+
     });
 }
 
